18f3c084399865c5c736f51e0c758a07859b4cd7
Solomon Aboyeji <solesty7@gmail.com>
2024-08-17 09:34:50 +0100

    added product analysis generation

--- src/helper.py
+++ src/helper.py
@@ -2,8 +2,9 @@ import json
 import os
 from pathlib import Path
 import pprint
+import re
 import time
-from typing import List, Optional, Sequence, Tuple
+from typing import Any, Dict, Iterator, List, Optional, Sequence, Tuple, TypeVar, Union
 from uuid import UUID, uuid4
 
 from sqlalchemy import text
@@ -48,15 +49,87 @@ from src.utils import StorageOption, filter_review_date
 from langchain_core.vectorstores import VectorStore
 
 from langchain_openai import ChatOpenAI
+from langchain.storage.exceptions import InvalidKeyException
 
 store_db_session = SessionLocal()
 
 MAXIMUM_GPT_ITERATIONS = 20
 
+from langchain_core.stores import BaseStore
 
-class CIAMDocumentFileStore(LocalFileStore):
 
-    def mset_docs(self, key_value_pairs: Sequence[Tuple[str, Document]]) -> None:
+class CIAMDocumentFileStore(BaseStore[str, Document]):
+
+    def __init__(
+        self,
+        root_path: Union[str, Path],
+        *,
+        chmod_file: Optional[int] = None,
+        chmod_dir: Optional[int] = None,
+        update_atime: bool = False,
+    ) -> None:
+        """Implement the BaseStore interface for the local file system.
+
+        Args:
+            root_path (Union[str, Path]): The root path of the file store. All keys are
+                interpreted as paths relative to this root.
+            chmod_file: (optional, defaults to `None`) If specified, sets permissions
+                for newly created files, overriding the current `umask` if needed.
+            chmod_dir: (optional, defaults to `None`) If specified, sets permissions
+                for newly created dirs, overriding the current `umask` if needed.
+            update_atime: (optional, defaults to `False`) If `True`, updates the
+                filesystem access time (but not the modified time) when a file is read.
+                This allows MRU/LRU cache policies to be implemented for filesystems
+                where access time updates are disabled.
+        """
+        self.root_path = Path(root_path).absolute()
+        self.chmod_file = chmod_file
+        self.chmod_dir = chmod_dir
+        self.update_atime = update_atime
+
+        """Initialize an empty store."""
+
+        self.store: Dict[str, Document] = {}
+
+        root_path_str = self.root_path
+        if type(self.root_path) == Path:
+            root_path_str = self.root_path.absolute()
+
+        for _path in os.listdir(root_path_str):
+            path = Path(_path)
+            if path.exists():
+                value = path.read_text()
+                content = json.loads(value)
+
+                document = Document(
+                    page_content=content["page_content"],
+                    id=content["id"],
+                    metadata=content["metadata"],
+                )
+                self.store[_path] = document
+
+    def _get_full_path(self, key: str) -> Path:
+        """Get the full path for a given key relative to the root path.
+
+        Args:
+            key (str): The key relative to the root path.
+
+        Returns:
+            Path: The full path for the given key.
+        """
+        if not re.match(r"^[a-zA-Z0-9_.\-/]+$", key):
+            raise InvalidKeyException(f"Invalid characters in key: {key}")
+        full_path = os.path.abspath(self.root_path / key)
+        common_path = os.path.commonpath([str(self.root_path), full_path])
+        if common_path != str(self.root_path):
+            raise InvalidKeyException(
+                f"Invalid key: {key}. Key should be relative to the full path."
+                f"{self.root_path} vs. {common_path} and full path of {full_path}"
+            )
+
+        return Path(full_path)
+
+    def mset(self, key_value_pairs: Sequence[Tuple[str, Document]]) -> None:
         """Set the values for the given keys.
 
         Args:
@@ -66,6 +139,8 @@ class CIAMDocumentFileStore(LocalFileStore):
             None
         """
         for key, value in key_value_pairs:
+            self.store[key] = value
+
             full_path = self._get_full_path(key)
             self._mkdir_for_store(full_path.parent)
             content = {
@@ -77,7 +152,7 @@ class CIAMDocumentFileStore(LocalFileStore):
             if self.chmod_file is not None:
                 os.chmod(full_path, self.chmod_file)
 
-    def mget_docs(self, keys: Sequence[str]) -> List[Optional[Document]]:
+    def mget(self, keys: Sequence[str]) -> List[Optional[Document]]:
         """Get the values associated with the given keys.
 
         Args:
@@ -87,27 +162,74 @@ class CIAMDocumentFileStore(LocalFileStore):
             A sequence of optional values associated with the keys.
             If a key is not found, the corresponding value will be None.
         """
+
         values: List[Optional[Document]] = []
         for key in keys:
-            full_path = self._get_full_path(key)
-            if full_path.exists():
-                value = full_path.read_text()
-                content = json.loads(value)
+            values.append(self.store[key])
 
-                values.append(
-                    Document(
-                        page_content=content["page_content"],
-                        id=content["id"],
-                        metadata=content["metadata"],
-                    )
-                )
-                if self.update_atime:
-                    # update access time only; preserve modified time
-                    os.utime(full_path, (time.time(), os.stat(full_path).st_mtime))
-            else:
-                values.append(Document(page_content="Empty Document", id=None))
+            # full_path = self._get_full_path(key)
+            # if full_path.exists():
+            #     value = full_path.read_text()
+            #     content = json.loads(value)
+
+            #     values.append(
+            #         Document(
+            #             page_content=content["page_content"],
+            #             id=content["id"],
+            #             metadata=content["metadata"],
+            #         )
+            #     )
+            #     if self.update_atime:
+            #         # update access time only; preserve modified time
+            #         os.utime(full_path, (time.time(), os.stat(full_path).st_mtime))
+            # else:
+            #     values.append(Document(page_content="Empty Document", id=None))
 
         return values
+
+    def _mkdir_for_store(self, dir: Path) -> None:
+        """Makes a store directory path (including parents) with specified permissions
+
+        This is needed because `Path.mkdir()` is restricted by the current `umask`,
+        whereas the explicit `os.chmod()` used here is not.
+
+        Args:
+            dir: (Path) The store directory to make
+
+        Returns:
+            None
+        """
+        if not dir.exists():
+            self._mkdir_for_store(dir.parent)
+            dir.mkdir(exist_ok=True)
+            if self.chmod_dir is not None:
+                os.chmod(dir, self.chmod_dir)
+
+    def mdelete(self, keys: Sequence[str]) -> None:
+        """Delete the given keys and their associated values.
+
+        Args:
+            keys (Sequence[str]): A sequence of keys to delete.
+        """
+        for key in keys:
+            if key in self.store:
+                del self.store[key]
+
+    def yield_keys(self, prefix: Optional[str] = None) -> Iterator[str]:  # type: ignore
+        """Get an iterator over keys that match the given prefix.
+
+        Args:
+            prefix (str, optional): The prefix to match. Defaults to None.
+
+        Yields:
+            Iterator[str]: An iterator over keys that match the given prefix.
+        """
+        if prefix is None:
+            yield from self.store.keys()
+        else:
+            for key in self.store.keys():
+                if key.startswith(prefix):
+                    yield key
 
 
 # Helper function to add documents into the vector and the doument store
@@ -154,7 +276,7 @@ def add_documents_no_split(
     assert len(docs) == len(parent_docs_contents)
 
     vectorstore.add_documents(docs, ids=product_ids)
-    docstore.mset_docs(list(zip(product_ids, parent_docs_contents)))
+    docstore.mset(list(zip(product_ids, parent_docs_contents)))
 
 
 # if product_combined_info_summaries:
@@ -213,7 +335,7 @@ def generate_embeddings(
     products_combined_infos = sorted(
         products_combined_infos.items(), key=lambda x: x[0]
     )
-
+    
     text_embedding_model_name = "nomic-embed-text"
     underlying_embedding = OpenAIEmbeddings()
 
